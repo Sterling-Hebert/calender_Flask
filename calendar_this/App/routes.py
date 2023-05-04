4cae065fbd342c7696193877bbcf4f679cc86de7
sterling_hebert <sterling_hebert@outlook.com>
2023-05-03 19:52:41 -0500

    still working on date conversion

--- calendar_this/App/routes.py
+++ calendar_this/App/routes.py
@@ -1,10 +1,23 @@
 from flask import Blueprint, render_template
-
+import os
+import sqlite3
 
 
 bp = Blueprint("main", __name__, url_prefix="/")
-
+DB_FILE = os.environ.get("DB_FILE")
 
 @bp.route("/")
 def main():
-    return render_template("main.html")
+    with sqlite3.connect(DB_FILE) as conn:
+        curs=conn.cursor()
+        curs.execute(
+        """
+        SELECT id, name, start_datetime, end_datetime
+        FROM appointments
+        ORDER BY start_datetime;
+        """
+        )
+        rows = curs.fetchall()
+
+        print(rows.start_datetime)
+    return render_template("main.html", rows=rows)
