50acd38cd1ea378336f9dcce82933e2b90df2a91
sterling_hebert <sterling_hebert@outlook.com>
2023-05-03 19:26:00 -0500

    First Page render

--- calendar_this/App/__init__.py
+++ calendar_this/App/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from App import routes
+from flask import Flask
+import os
 
 app = Flask(__name__)
-
-app.config.from_object(Config)
+app.config.update({'SECRET_KEY': os.environ.get('SECRET_KEY')})
+app.register_blueprint(routes.bp)
